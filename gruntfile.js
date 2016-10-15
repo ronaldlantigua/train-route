f60636791d8e78b5cfd2d2076d7826e87133c524
Ronald Lantigua <rlantigua@intellisys.com.do>
2016-10-15 12:11:31 -0400

    added less config on grunt file

---
+++ gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+  //Registering module
+  grunt.loadNpmTasks('grunt-contrib-less');
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "build/css/main.css": "less/main.less" // destination file and source file
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['less/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  // A very basic default task.
+  grunt.registerTask('default', ['less', 'watch']);
+
+};
